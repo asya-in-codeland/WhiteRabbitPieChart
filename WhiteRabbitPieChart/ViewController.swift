20b5f3fcc459903d6180bafebcb98d07b79e2455
asya-in-codeland <asya.in.codeland@gmail.com>
2017-11-14 19:36:24 +0300

    Initial Commit

---
+++ WhiteRabbitPieChart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WhiteRabbitPieChart
+//
+//  Created by Anastasia Sviridenko on 14/11/2017.
+//  Copyright © 2017 ASya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
